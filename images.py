2619739514f0a36fe040795b2ba1e129c5b8f52d
cromewar <vasiliy@cromewar.com>
2021-05-17 08:15:13 -0500

    removing unnesary files

--- images.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image, ImageFilter
-
-img = Image.open('./astro.jpg')
-new_image = img.resize((400,400))
-new_image.save('thumbnail.jpg')
---
+++ jpg_to_png.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import sys
+import os
+
+# grab arguments here
+
+# check if folder exists, if not create a new open
+
+# loop through folder and transform to png
+
+#Save all transformations on the new folder
