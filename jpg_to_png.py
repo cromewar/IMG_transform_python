3c990de16ebd27518e91d9d38f65073e51ba0db1
cromewar <vasiliy@cromewar.com>
2021-05-17 09:45:56 -0500

    standard file transforming and Sample Img

--- jpg_to_png.py
+++ jpg_to_png.py
@@ -5,6 +5,8 @@ import os
 # grab arguments here
 img_folder = sys.argv[1]
 output_folder = sys.argv[2]
+file_format = sys.argv[3]
+
 #checks if the primary folder exists
 if not os.path.exists(img_folder):
     print(f'the folder {img_folder} does not exists')
@@ -14,11 +16,14 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 # loop through folder and transform to png
 for file in os.listdir(img_folder):
-    img = Image.open(f'{img_folder}{file}')
-    remove_file_extension = os.path.splitext(file)[0]
-    img.save(f'{output_folder}{remove_file_extension}.png', 'png')
+    try:
+        img = Image.open(f'{img_folder}{file}')
+        remove_file_extension = os.path.splitext(file)[0]
+        img.save(f'{output_folder}{remove_file_extension}.{file_format}', file_format)
+    except OSError as err:
+        print('One or more files can\'t be transformed')
+
+
 
 print('all images has been processed')
 
-
-#Save all transformations on the new folder
